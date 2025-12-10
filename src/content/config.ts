6391b4690339d5fcbf9daf9c068f017341c4bbed
Juan Cruz <juanmir227@gmail.com>
2025-12-10 01:18:56 -0300

    fixed lo de drafts. Esta listo para empezar a subir posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
 import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
-  type: "content",
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
     description: z.string(),
+    pubDate: z.date(),
+    category: z.string().optional(),
+    draft: z.boolean().optional(), // ⬅️ NECESARIO
   }),
 });
 
